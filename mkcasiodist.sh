54e5295ed5e550ed674a8f70bf1832cfa01d7373
ComputerNerd <theprogrammernerd@gmail.com>
2021-04-24 04:42:51 -0500

    Also generate a zip file. Note that this is slow because the iterations are over 9000 for Zopfli.

--- mkcasiodist.sh
+++ mkcasiodist.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 rm -f openjazz.7z
 7z a -t7z -m0=lzma -mx=9 -ms=on openjazz.7z openjazz.g3a Changelog README
-
+# Cemetech requires a zip file.
+rm -f openjazz.zip
+advzip -a -i 9001 -4 openjazz.zip openjazz.g3a Changelog README
